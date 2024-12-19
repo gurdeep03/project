b0b1170fed287b46741dbcbe26970bf34ec363fd
Gurdeep082 <sainigurdeep082@gmail.com>
2024-12-19 21:15:28 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(cors({ origin: 'http://localhost:3001', // Allow requests from this origin
  }));
 
+ 
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
